f4218a3cc84fe7edfac02b8a966778a3dfd1a2bb
Nicolas Gabriel Sanchez <nicosanchezdeveloper@gmail.com>
2024-07-01 20:44:59 -0300

    Add routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { KnifeCatalogComponent } from './knife-catalog/knife-catalog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path: '',
+  component: AboutComponent
+},
+{
+  path: 'knifecatalog',
+  component: KnifeCatalogComponent
+}
+,
+{
+  path: 'contact',
+  component: ContactComponent
+}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
